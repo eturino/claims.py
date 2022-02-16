a2dd0a9d7a0eaafa45aacca268d003e45338fd42
Eduardo Turiño <eturino@eturino.com>
2022-02-16 19:28:22 +0100

    fix: add key_set as dependency

--- setup.py
+++ setup.py
@@ -40,7 +40,9 @@ setup(
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     python_requires=">= 3.6",
-    install_requires=[],
+    install_requires=[
+        "key_set",
+    ],
     # Licensing and copyright
     license="Apache 2.0",
 )
